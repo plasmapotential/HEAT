57bb0a079dcf21b8118dbfa8adfec3f2bb6caf0d
plasmapotential <thomaslooby@gmail.com>
2022-11-03 13:51:13 -0400

    readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,11 +26,14 @@ release = '3.0'
 
 
 # -- General configuration ---------------------------------------------------
-
+import sphinx_rtd_theme
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['python_docs_theme']
+extensions = [
+    ...
+    'sphinx_rtd_theme',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -46,7 +49,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'python_docs_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
