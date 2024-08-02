4102b9394ec3a7bf91b1aedb72db39025687e453
EJCT22 <etinacba@nip.upd.edu.ph>
2024-08-02 15:31:23 -0400

    debugging rzqprofile for TUI

--- source/engineClass.py
+++ source/engineClass.py
@@ -1056,6 +1056,7 @@ class engineObj():
             print("saving rzq file to tmpDir.")
             self.HF.rzqFile = self.HF.writerzqFileData(rzqFile, rzqFiledata, self.tmpDir)
         else:
+            print(rzqFile)
             self.HF.rzqFile = rzqFile
 
         self.HF.readrzqprofile(self.HF.rzqFile)
